a3cdcfafa1508e6c3e7ae73b5a73df52c9c4f0ac
Niyaz Garipov <Niayz_Indicus@mail.ru>
2018-06-28 15:24:12 +0300

    Creates dictionaries and connects form-ad.js to index.html

--- js/form-ad.js
+++ js/form-ad.js
@@ -1,1 +1,15 @@
 'use strict';
+// Создаём словари
+var realEstateTypeToMinPrice = {
+  'bungalo': 0,
+  'flat': 1000,
+  'house': 5000,
+  'palace': 10000
+};
+
+var roomToGuest = {
+  '1': [1],
+  '2': [1, 2],
+  '3': [1, 2, 3],
+  '100': [0]
+};
