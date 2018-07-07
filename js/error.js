01973fb8d650188c7e81591485159895e0a803f3
Niyaz Garipov <Niayz_Indicus@mail.ru>
2018-07-07 13:31:58 +0300

    Creates DOM-element that displays an error message

--- js/error.js
+++ js/error.js
@@ -1,1 +1,15 @@
 'use strict';
+
+(function () {
+  var node = document.createElement('div');
+
+  // Создаём DOM-элемент, показывающий сообщение об ошибке
+  window.createErrorMessage = function (text) {
+    node.classList.add('error');
+    node.textContent = text;
+    document.body.appendChild(node);
+    setTimeout(function () {
+      document.body.removeChild(node);
+    }, 5000);
+  };
+})();
