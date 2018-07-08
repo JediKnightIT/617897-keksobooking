93d17cd1554e997c80c987ef36895353e949883b
Niyaz Garipov <Niayz_Indicus@mail.ru>
2018-07-08 23:04:36 +0300

    Creates filter.js and data in it

---
+++ js/filter.js
@@ -0,0 +1,21 @@
+'use strict';
+
+(function () {
+  // Создаём данные
+  var priceRange = {
+    low: {
+      MIN: 0,
+      MAX: 9999
+    },
+    middle: {
+      MIN: 10000,
+      MAX: 50000
+    },
+    high: {
+      MIN: 50001,
+      MAX: Infinity
+    }
+  };
+
+  var PIN_QUANTITY = 5;
+})();
