953aa90a5b6909660d2e114610431c3e33f2b62f
Niyaz Garipov <Niayz_Indicus@mail.ru>
2018-07-08 17:27:05 +0300

    Changes variable to constant

--- js/error.js
+++ js/error.js
@@ -2,7 +2,7 @@
 
 (function () {
   var node = document.createElement('div');
-  var timeout = 5000;
+  var TIMEOUT = 5000;
   // Создаём DOM-элемент, показывающий сообщение об ошибке
   var createErrorMessage = function (text) {
     node.classList.add('error');
@@ -10,7 +10,7 @@
     document.body.appendChild(node);
     setTimeout(function () {
       document.body.removeChild(node);
-    }, timeout);
+    }, TIMEOUT);
   };
 
   window.error = {
