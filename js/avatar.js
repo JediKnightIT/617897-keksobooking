eeacb71d4d70626529332c0a652451f450bfbff2
Niyaz Garipov <Niayz_Indicus@mail.ru>
2018-07-11 20:37:13 +0300

    Findes elements for uploading and displaying avatar

---
+++ js/avatar.js
@@ -0,0 +1,8 @@
+'use strict';
+
+(function () {
+  // Находим элементы в разметке и присваиваем их переменным
+  var avatarPreview = document.querySelector('.ad-form-header__preview');
+  var fileChooser = document.querySelector('.ad-form__field input[type=file]');
+
+})();
