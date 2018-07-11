399e38a54be6e21cc134ae2ad4152594aa27d1a1
Niyaz Garipov <Niayz_Indicus@mail.ru>
2018-07-11 21:22:50 +0300

    Checks downloaded files

--- js/avatar.js
+++ js/avatar.js
@@ -8,4 +8,12 @@
   var avatarPreview = document.querySelector('.ad-form-header__preview img');
   var fileChooser = document.querySelector('.ad-form__field input[type=file]');
 
+  // Добавляем обработчик события изменения состояния fileChooser
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    if (checkFileType) {
+    }
+  });
 })();
