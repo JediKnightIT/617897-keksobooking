1c56308c176f9e23c8fe55baa796fa266fbb27fd
Niyaz Garipov <Niayz_Indicus@mail.ru>
2018-07-11 21:20:16 +0300

    Creates array

--- js/avatar.js
+++ js/avatar.js
@@ -1,8 +1,11 @@
 'use strict';
 
 (function () {
+  // Создаём структуру данных
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
   // Находим элементы в разметке и присваиваем их переменным
-  var avatarPreview = document.querySelector('.ad-form-header__preview');
+  var avatarPreview = document.querySelector('.ad-form-header__preview img');
   var fileChooser = document.querySelector('.ad-form__field input[type=file]');
 
 })();
