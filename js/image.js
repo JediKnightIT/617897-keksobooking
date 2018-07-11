c89f638e238cc66611e4d556010c3fade6ea56ee
Niyaz Garipov <Niayz_Indicus@mail.ru>
2018-07-11 23:55:21 +0300

    Rewrites avatar download code

--- js/image.js
+++ js/image.js
@@ -4,28 +4,48 @@
   // Создаём структуру данных
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
-  // Находим элементы в разметке и присваиваем их переменным
-  var avatarPreview = document.querySelector('.ad-form-header__preview img');
-  var fileChooser = document.querySelector('.ad-form__field input[type=file]');
+  var imageData = {
+    WIDTH: '70',
+    HEIGHT: '70',
+    PADDING: 'padding: 0;',
+    BORDER: 'border-radius: 5px;'
+  };
 
-  // Добавляем обработчик события изменения состояния fileChooser
-  fileChooser.addEventListener('change', function () {
-    var file = fileChooser.files[0];
+  // Находим элементы в разметке и присваиваем их переменным
+  var adForm = document.querySelector('.ad-form');
+  var avatarPreviewContainer = adForm.querySelector('.ad-form-header__preview');
+  var avatarPreview = adForm.querySelector('.ad-form-header__preview img');
+  var avatar = adForm.querySelector('#avatar');
+
+  // Функция, загружающая аватарку с локальной диска
+  var loadAvatar = function () {
+    var file = avatar.files[0];
     var fileName = file.name.toLowerCase();
 
     // Проверяем тип загружаемого файла
-    var checkFileType = FILE_TYPES.some(function (it) {
+    var matches = FILE_TYPES.some(function (it) {
       return fileName.endsWith(it);
     });
 
-    // Получакм исходный код загружаемого файла
-    if (checkFileType) {
+    if (matches) {
       var fileReader = new FileReader();
 
       fileReader.addEventListener('load', function () {
         avatarPreview.src = fileReader.result;
+        avatarPreview.width = imageData.WIDTH;
+        avatarPreview.height = imageData.HEIGHT;
+        avatarPreview.style = imageData.BORDER;
+        avatarPreviewContainer.style = imageData.PADDING;
       });
       fileReader.readAsDataURL(file);
     }
-  });
+  };
+
+  // Функция-обработчик, загружающая аватарку пользователя
+  var onInputChange = function () {
+    loadAvatar();
+  };
+
+  // Добавляем обработчик события change
+  avatar.addEventListener('change', onInputChange);
 })();
